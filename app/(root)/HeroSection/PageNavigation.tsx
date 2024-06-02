ab5872d9e87382882e3727c9566d7572af195e37
keshav-aham <keshav.takatrao@aham.health>
2024-06-02 21:29:53 +0530

    add hero background gradient

--- app/(root)/HeroSection/PageNavigation.tsx
+++ app/(root)/HeroSection/PageNavigation.tsx
@@ -3,8 +3,8 @@ import Link from "next/link";
 
 export default function PageNavigation() {
   return (
-    <div className="hidden lg:block absolute bottom-0 translate-y-1/2 left-0 w-full">
-      <div className="flex items-center mx-auto w-fit px-16 py-2 rounded-full drop-shadow-md">
+    <div className="hidden lg:block absolute bottom-0 translate-y-1/2 left-0 w-full ">
+      <div className="flex items-center mx-auto w-fit px-16 py-2 rounded-full bg-white drop-shadow-md">
         {PAGE_NAVIGATION.map((item) => (
           <Link
             className="hover:underline underline-offset-4 px-4 py-2"
--- app/(root)/HeroSection/index.tsx
+++ app/(root)/HeroSection/index.tsx
@@ -8,7 +8,9 @@ const lora = Lora({ subsets: ["latin"], weight: "700" });
 
 export default function HeroSection() {
   return (
-    <section className="lg:py-[60px] py-7">
+    <section className="lg:py-[60px] py-7 relative bg-blend-color-burn">
+      <div className="bg-[linear-gradient(90deg,#fff_25%,rgba(255,221,19,0.4)_50%,rgba(110,_38,_36,_0.30)_75%,_rgba(72,_36,_110,_0.30)_100%)] blur-[250px] absolute inset-0 -z-40"></div>
+      {/* <div className="bg-[linear-gradient(90deg,#fff_0%,rgba(110,_38,_36,_0.30)_75%,_rgba(72,_36,_110,_0.30)_100%)] bg-blend-overlay backdrop-blur-[100px] absolute inset-0 -z-50"></div> */}
       <div className="container relative px-4">
         <div className="flex flex-col lg:flex-row lg:items-center justify-between">
           <div className="space-y-5 max-w-xl">
@@ -30,6 +32,7 @@ export default function HeroSection() {
             width={500}
             height={500}
             alt="Hero Image"
+            className="rounded-full"
           />
         </div>
         <PageNavigation />
